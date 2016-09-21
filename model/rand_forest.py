b86d91ac8ec5823184a7f292fc077354e7528241
xuguanggen <821439322@qq.com>
2016-09-22 00:47:42 +0800

    add some nearest feature to train rf1.2

--- model/rand_forest.py
+++ model/rand_forest.py
@@ -9,7 +9,7 @@ from sklearn.ensemble import RandomForestRegressor
 
 import sys
 sys.path.append('..')
-from preprocess.config import front_num_points,last_num_points
+from preprocess.config import front_num_points,last_num_points,num_neighbors
 from preprocess.config import train_csv_path,test_csv_path
 
 
@@ -24,6 +24,18 @@ for i in range(last_num_points):
     fields.append('Last_LON_'+str(i+1))
     fields.append('Last_LAT_'+str(i+1))
 
+
+### add nearest num_neighbors destination ######
+for i in range(num_neighbors):
+    fields.append('Nearest_LON_'+str(i+1))
+    fields.append('Nearest_LAT_'+str(i+1))
+
+### add nearest num_neighbors odistance ######
+for i in range(num_neighbors):
+    fields.append('Nearest_Edistance'+str(i+1))
+
+
+
 fields.append('TAXI_ID')
 fields.append('DAYOFWEEK')
 fields.append('HOUROFDAY')
@@ -35,31 +47,51 @@ fields.append('TRJ_SPEED')
 
 
 
+def getListFeature(df,fieldsname):
+    sub_feature = []
+    for i in range(len(df)):
+        cur_list = list(eval(df[fieldsname][i]))
+        cur_list = np.array(cur_list)
+        cur_list = cur_list.flatten()
+        
+        cur_feature = []
+        for j in range(len(cur_list)):
+            cur_feature.append(float(cur_list[j]))
+        sub_feature.append(cur_feature)
+
+    return np.array(sub_feature)
+
+
 
 def load_data(csv_path,isTrain):
     df = pd.read_csv(csv_path,header = 0)
 
-    front_part_trj_list = []
-    last_part_trj_list = []
-    for i in range(len(df)):
-        cur_front_part_trj = list(eval(df['TRJ_FRONT_POINTS'][i]))
-        cur_last_part_trj = list(eval(df['TRJ_LAST_POINTS'][i]))
-        cur_lonlat_front_list = []
-        cur_lonlat_last_list = []
-        for j in range(len(cur_front_part_trj)):
-            cur_lonlat_front_list.append(float(cur_front_part_trj[j][0]))
-            cur_lonlat_front_list.append(float(cur_front_part_trj[j][1]))
+    #front_part_trj_list = []
+    #last_part_trj_list = []
+    #nearest_trj_destination_list = []
+    #nearest_trj_edist_list = []
+    #for i in range(len(df)):
+    #    cur_front_part_trj = list(eval(df['TRJ_FRONT_POINTS'][i]))
+    #    cur_last_part_trj = list(eval(df['TRJ_LAST_POINTS'][i]))
+    #    cur_nearest_trj_destination = list(eval(df['']))
+    #    cur_lonlat_front_list = []
+    #    cur_lonlat_last_list = []
+    #    for j in range(len(cur_front_part_trj)):
+    #        cur_lonlat_front_list.append(float(cur_front_part_trj[j][0]))
+    #        cur_lonlat_front_list.append(float(cur_front_part_trj[j][1]))
 
-        for j in range(len(cur_last_part_trj)):
-            cur_lonlat_last_list.append(float(cur_last_part_trj[j][0]))
-            cur_lonlat_last_list.append(float(cur_last_part_trj[j][1]))
-        front_part_trj_list.append(cur_lonlat_front_list)
-        last_part_trj_list.append(cur_lonlat_last_list)
+    #    for j in range(len(cur_last_part_trj)):
+    #        cur_lonlat_last_list.append(float(cur_last_part_trj[j][0]))
+    #        cur_lonlat_last_list.append(float(cur_last_part_trj[j][1]))
+    #    front_part_trj_list.append(cur_lonlat_front_list)
+    #    last_part_trj_list.append(cur_lonlat_last_list)
     
-    feature_frontTrj = np.array(front_part_trj_list)
-    feature_last_Trj = np.array(last_part_trj_list)
+    feature_frontTrj = getListFeature(df,'TRJ_FRONT_POINTS')
+    feature_last_Trj = getListFeature(df,'TRJ_LAST_POINTS')
+    feature_nearest_destination = getListFeature(df,'NEIGHBORS_DESTINATION')
+    feature_nearest_odistance = getListFeature(df,'NEIGHBORS_Euclidean_DISTANCE')
 
-    data_x = np.hstack([feature_frontTrj,feature_last_Trj])
+    data_x = np.hstack([feature_frontTrj,feature_last_Trj,feature_nearest_destination,feature_nearest_odistance])
     for i in range( (front_num_points + last_num_points )*2,len(fields)):
         cur_feature_array = np.array(df[fields[i]]).reshape(data_x.shape[0],1)
         data_x = np.hstack([data_x,cur_feature_array])
@@ -111,7 +143,7 @@ def run(result_csv_path):
 
 if __name__=='__main__':
     start = time()
-    result_csv_path = 'result/rf_20160919_1.1.csv'
+    result_csv_path = 'result/rf_20160922_1.2.csv'
     run(result_csv_path)
     end = time()
     print('Time :\t'+str((end - start)/3600) +' Hours')
