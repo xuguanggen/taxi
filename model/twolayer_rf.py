a4979a41bda7ccc54323c06b494989c51bc1b6f2
xuguanggen <ggxu.soochow@gmail.com>
2016-09-26 00:33:54 +0800

    complete half 2 layer rf coding

--- model/twolayer_rf.py
+++ model/twolayer_rf.py
@@ -8,90 +8,40 @@ import pandas as pd
 from sklearn.externals import joblib
 from sklearn.ensemble import RandomForestRegressor
 
+from utils import load_data
+
 import sys
 sys.path.append('..')
 from preprocess.config import front_num_points,last_num_points,num_neighbors
 from preprocess.config import train_csv_path,test_csv_path
+from preprocess.config import fields
+
 
 
 Model_Name = 'TwoLayerRF_20160925_1'
 
 
 
-
-fields = []
-for i in range(front_num_points):
-    fields.append('Front_LON_'+str(i+1))
-    fields.append('Front_LAT_'+str(i+1))
-
-for i in range(last_num_points):
-    fields.append('Last_LON_'+str(i+1))
-    fields.append('Last_LAT_'+str(i+1))
-
-
-### add nearest num_neighbors destination ######
-for i in range(num_neighbors):
-    fields.append('Nearest_LON_'+str(i+1))
-    fields.append('Nearest_LAT_'+str(i+1))
-
-### add nearest num_neighbors odistance ######
-for i in range(num_neighbors):
-    fields.append('Nearest_Edistance'+str(i+1))
+def feature_engineer(feature,pred):
 
 
 
-fields.append('TAXI_ID')
-fields.append('DAYOFWEEK')
-fields.append('HOUROFDAY')
-fields.append('CALL_TYPE')
-fields.append('DAY_TYPE')
-fields.append('TRJ_Haversine_DISTANCE')
-fields.append('TRJ_Euclidean_DISTANCE')
-fields.append('IS_AWAY_CENTER')
-fields.append('TRJ_TIME')
-fields.append('TRJ_SPEED')
+
+def run(result_csv_path):
+    train_x,train_y = load_data(train_csv_path,True)
+    test_x = load_data(test_csv_path,False)
+    print('load data successfully.........')
+
+    layer1_rf = RandomForestRegressor(
+            n_estimators = 2500,
+            max_features = 0.8,
+            bootstrap = False,
+            max_depth = 15,
+            n_jobs = -1
+            )
+    layer1_rf.fit(train_x,train_y)
+    print('layer 1 train successfully..........')
+    ################# save model##################
+    joblib.dump(layer1_rf,'weights/layer1_'+Model_Name+'.m')
 
 
-
-def getListFeature(df,fieldsname):
-    sub_feature = []
-    for i in range(len(df)):
-        cur_list = list(eval(df[fieldsname][i]))
-        cur_list = np.array(cur_list)
-        cur_list = cur_list.flatten()
-        
-        cur_feature = []
-        for j in range(len(cur_list)):
-            cur_feature.append(float(cur_list[j]))
-        sub_feature.append(cur_feature)
-
-    return np.array(sub_feature)
-
-
-
-def load_data(csv_path,isTrain):
-    df = pd.read_csv(csv_path,header = 0)
-    
-    feature_frontTrj = getListFeature(df,'TRJ_FRONT_POINTS')
-    feature_last_Trj = getListFeature(df,'TRJ_LAST_POINTS')
-    feature_nearest_destination = getListFeature(df,'NEIGHBORS_DESTINATION')
-    feature_nearest_odistance = getListFeature(df,'NEIGHBORS_Euclidean_DISTANCE')
-
-    data_x = np.hstack([feature_frontTrj,feature_last_Trj,feature_nearest_destination,feature_nearest_odistance])
-    for i in range( (front_num_points + last_num_points )*2 + num_neighbors*3,len(fields)):
-        cur_feature_array = np.array(df[fields[i]]).reshape(data_x.shape[0],1)
-        data_x = np.hstack([data_x,cur_feature_array])
-
-
-    if isTrain:
-        train_y = []
-        for i in range(len(df)):
-            train_y.append(list(eval(df['DESTINATION'][i])))
-        train_y = np.array(train_y)
-        return data_x,train_y
-    else:
-        return data_x
-
-
-
-def 
--- preprocess/config.py
+++ preprocess/config.py
@@ -8,3 +8,6 @@ num_neighbors = 10
 
 train_csv_path ="/volume1/xuguanggen/competition/taxi/data/clean_data/clean_train.csv"
 test_csv_path ="/volume1/xuguanggen/competition/taxi/data/clean_data/clean_test.csv"
+
+dict_calltype = {'A':0,'B':1,'C':2}
+dict_daytype = {'A':0,'B':1,'C':2}
