f870d1dc9dd46a12af06a39e3e756e1b7c097e65
xuguanggen <ggxu.soochow@gmail.com>
2016-09-26 09:35:20 +0800

    整合rand_forest.py

--- model/rand_forest.py
+++ model/rand_forest.py
@@ -8,6 +8,10 @@ import pandas as pd
 from sklearn.externals import joblib
 from sklearn.ensemble import RandomForestRegressor
 
+
+from utils import load_data
+from utils import save_results
+
 import sys
 sys.path.append('..')
 from preprocess.config import front_num_points,last_num_points,num_neighbors
@@ -15,100 +19,6 @@ from preprocess.config import train_csv_path,test_csv_path
 
 
 Model_Name = 'rf_20160925_1.3'
-
-
-fields = []
-for i in range(front_num_points):
-    fields.append('Front_LON_'+str(i+1))
-    fields.append('Front_LAT_'+str(i+1))
-
-for i in range(last_num_points):
-    fields.append('Last_LON_'+str(i+1))
-    fields.append('Last_LAT_'+str(i+1))
-
-
-### add nearest num_neighbors destination ######
-for i in range(num_neighbors):
-    fields.append('Nearest_LON_'+str(i+1))
-    fields.append('Nearest_LAT_'+str(i+1))
-
-### add nearest num_neighbors odistance ######
-for i in range(num_neighbors):
-    fields.append('Nearest_Edistance'+str(i+1))
-
-
-
-fields.append('TAXI_ID')
-fields.append('DAYOFWEEK')
-fields.append('HOUROFDAY')
-fields.append('CALL_TYPE')
-fields.append('DAY_TYPE')
-fields.append('TRJ_Haversine_DISTANCE')
-fields.append('TRJ_Euclidean_DISTANCE')
-fields.append('IS_AWAY_CENTER')
-fields.append('TRJ_TIME')
-fields.append('TRJ_SPEED')
-
-
-
-def getListFeature(df,fieldsname):
-    sub_feature = []
-    for i in range(len(df)):
-        cur_list = list(eval(df[fieldsname][i]))
-        cur_list = np.array(cur_list)
-        cur_list = cur_list.flatten()
-        
-        cur_feature = []
-        for j in range(len(cur_list)):
-            cur_feature.append(float(cur_list[j]))
-        sub_feature.append(cur_feature)
-
-    return np.array(sub_feature)
-
-
-
-def load_data(csv_path,isTrain):
-    df = pd.read_csv(csv_path,header = 0)
-
-    #front_part_trj_list = []
-    #last_part_trj_list = []
-    #nearest_trj_destination_list = []
-    #nearest_trj_edist_list = []
-    #for i in range(len(df)):
-    #    cur_front_part_trj = list(eval(df['TRJ_FRONT_POINTS'][i]))
-    #    cur_last_part_trj = list(eval(df['TRJ_LAST_POINTS'][i]))
-    #    cur_nearest_trj_destination = list(eval(df['']))
-    #    cur_lonlat_front_list = []
-    #    cur_lonlat_last_list = []
-    #    for j in range(len(cur_front_part_trj)):
-    #        cur_lonlat_front_list.append(float(cur_front_part_trj[j][0]))
-    #        cur_lonlat_front_list.append(float(cur_front_part_trj[j][1]))
-
-    #    for j in range(len(cur_last_part_trj)):
-    #        cur_lonlat_last_list.append(float(cur_last_part_trj[j][0]))
-    #        cur_lonlat_last_list.append(float(cur_last_part_trj[j][1]))
-    #    front_part_trj_list.append(cur_lonlat_front_list)
-    #    last_part_trj_list.append(cur_lonlat_last_list)
-    
-    feature_frontTrj = getListFeature(df,'TRJ_FRONT_POINTS')
-    feature_last_Trj = getListFeature(df,'TRJ_LAST_POINTS')
-    feature_nearest_destination = getListFeature(df,'NEIGHBORS_DESTINATION')
-    feature_nearest_odistance = getListFeature(df,'NEIGHBORS_Euclidean_DISTANCE')
-
-    data_x = np.hstack([feature_frontTrj,feature_last_Trj,feature_nearest_destination,feature_nearest_odistance])
-    for i in range( (front_num_points + last_num_points )*2 + num_neighbors*3,len(fields)):
-        cur_feature_array = np.array(df[fields[i]]).reshape(data_x.shape[0],1)
-        data_x = np.hstack([data_x,cur_feature_array])
-
-
-    if isTrain:
-        train_y = []
-        for i in range(len(df)):
-            train_y.append(list(eval(df['DESTINATION'][i])))
-        train_y = np.array(train_y)
-        return data_x,train_y
-    else:
-        return data_x
 
 
 
@@ -132,13 +42,7 @@ def run(result_csv_path):
 
 
     ####### save_results ###########################
-    df = pd.read_csv(test_csv_path,header = 0)
-    result = pd.DataFrame()
-    result['TRIP_ID'] = df['TRIP_ID']
-    result['LATITUDE'] = y_pred[:,1]
-    result['LONGITUDE'] = y_pred[:,0]
-    result.to_csv(result_csv_path,index = False)
-
+    save_results(result_csv_path,y_pred)
 
     ###### generate report #######################
     feature_importances = rf.feature_importances_
